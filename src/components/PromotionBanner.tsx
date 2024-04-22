4417a9c8866e012314d6e5e189c9c77a75111fd9
bonkizu <isukaomu@gmail.com>
2024-04-22 13:17:18 +0700

    car and promotion more responsive

--- src/components/PromotionBanner.tsx
+++ src/components/PromotionBanner.tsx
@@ -3,15 +3,16 @@ import SavedSearchIcon from "@mui/icons-material/SavedSearch";
 import Link from "next/link";
 
 export default function PromotionBanner({ promotion }: { promotion: string }) {
-
   const mockId = [
-    { picture:`/promotions/0.jpg`,id:"66251f14c9c59cdaa789d8d8" },
-    { picture:`/promotions/1.jpg`,id:"66251faac9c59cdaa789d8df" },
-    { picture:`/promotions/2.jpg`,id:"66252030c9c59cdaa789d8e4" }
+    { picture: `/promotions/0.jpg`, id: "66251f14c9c59cdaa789d8d8" },
+    { picture: `/promotions/1.jpg`, id: "66251faac9c59cdaa789d8df" },
+    { picture: `/promotions/2.jpg`, id: "66252030c9c59cdaa789d8e4" },
   ];
 
-  const foundPromotion = mockId.find(item => item.picture === `/promotions/${promotion}.jpg`);
-  const promotionId = foundPromotion?.id || ''; 
+  const foundPromotion = mockId.find(
+    (item) => item.picture === `/promotions/${promotion}.jpg`
+  );
+  const promotionId = foundPromotion?.id || "";
 
   return (
     <div className="flex items-center justify-center">
@@ -21,15 +22,18 @@ export default function PromotionBanner({ promotion }: { promotion: string }) {
         width={0}
         height={0}
         sizes="100vw"
-        className="h-[400px] w-auto p-12"
+        className="h-[200px] sm:h-[280px] md:h-[350px] lg:h-[400px] w-auto p-12"
       />
-      <div className="bg-[#fb7dc0] text-white text-nowrap py-2 px-8 rounded-lg absolute mt-[200px]">
+      <div className="bg-[#fb7dc0] text-white font-bold text-nowrap py-2 px-4 rounded-lg absolute mt-[50px] sm:mt-[100px] md:mt-[180px] lg:mt-[220px] md:py-4 md:px-8 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-150">
         <SavedSearchIcon />
-        <Link href={`/promotion/${promotionId}`} prefetch={true} className="ml-2">
+        <Link
+          href={`/promotion/${promotionId}`}
+          prefetch={true}
+          className="ml-2"
+        >
           Check Out
         </Link>
       </div>
     </div>
   );
 }
-
