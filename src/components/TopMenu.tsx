34a51ebb7220d85ddd31459c0d7228178434e5c8
Phuree <phureeppp@gmail.com>
2024-04-25 21:43:01 +0700

    responsive navbar1

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -22,13 +22,13 @@ export default function TopMenu() {
     <div
       className={`w-[100vw] flex-nowrap flex h-[65px] items-center bg-white shadow justify-between fixed top-0 z-[100] transition-transform duration-900`}
     >
-      <div className="flex items-center justify-center w-14 h-14  py-2 absolute bg-pink-400 rounded-full ml-[80px] text-white text-[1rem] font-[500] z-50">
+      <div className="ml-[5%] lg:ml-[80px] flex items-center justify-center w-14 h-14  py-2 absolute bg-pink-400 rounded-full  text-white text-[1rem] font-[500] z-50">
         <p className="flex flex-col items-center select-none hover:cursor-default">
           <span>キム</span>
           <span>先生</span>
         </p>
       </div>
-      <div className="absolute flex items-center justify-center w-full h-full text-3xl font-normal text-center text-black">
+      <div className="absolute flex items-center justify-center w-full h-full text-xl font-normal text-center text-black lg:text-3xl">
         <TopMenuItem
           customClasses="z-50 hover:opacity-35 transition-opacity"
           item={
@@ -45,7 +45,7 @@ export default function TopMenu() {
         {session.data && (
           <TopMenuItem
             item={
-              <div onClick={(e) => signOut()}>
+              <div onClick={(e) => signOut()} className="hidden lg:block">
                 <LogoutIcon sx={{ color: iconColor, fontSize: 45 }} />
               </div>
             }
@@ -60,7 +60,7 @@ export default function TopMenu() {
               height={0}
               sizes="100vh"
               alt="car-icon"
-              className="w-[35px] h-[28px]"
+              className="w-[35px] h-[28px] hidden lg:block"
               src="/img/car-icon.png"
             />
           }
@@ -74,12 +74,71 @@ export default function TopMenu() {
               height={0}
               sizes="100vh"
               alt="profile-icon"
-              className="w-[35px] h-[35px]"
+              className="w-[35px] h-[35px] hidden lg:block"
               src="/img/profile-icon.png"
             />
           }
           pageRef="/user"
         />
+        <div className="dropdown dropdown-hover lg:hidden">
+          <Image
+            src="/img/menu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            className="cursor-pointer min-w-[32px]"
+            tabIndex={0}
+          />
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <TopMenuItem
+                customClasses="z-50 hover:scale-105"
+                item={
+                  <Image
+                    width={0}
+                    height={0}
+                    sizes="100vh"
+                    alt="car-icon"
+                    className="w-[35px] h-[28px]"
+                    src="/img/car-icon.png"
+                  />
+                }
+                pageRef="/cars"
+              />
+            </li>
+            <li>
+              <TopMenuItem
+                customClasses="z-50 hover:scale-105"
+                item={
+                  <Image
+                    width={0}
+                    height={0}
+                    sizes="100vh"
+                    alt="profile-icon"
+                    className="w-[35px] h-[35px]"
+                    src="/img/profile-icon.png"
+                  />
+                }
+                pageRef="/user"
+              />
+            </li>
+            <li>
+              {session.data && (
+                <TopMenuItem
+                  item={
+                    <div onClick={(e) => signOut()}>
+                      <LogoutIcon sx={{ color: iconColor, fontSize: 45 }} />
+                    </div>
+                  }
+                  pageRef="/"
+                />
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
