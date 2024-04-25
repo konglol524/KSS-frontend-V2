898497ba9bf8b5f2da4a1a0533bf5a49b9d55178
PiyawatArt <piyawat.manu@gmail.com>
2024-04-25 10:05:18 +0700

    add username in comment

--- src/app/(promotioninfo)/promotion/[pid]/page.tsx
+++ src/app/(promotioninfo)/promotion/[pid]/page.tsx
@@ -24,7 +24,7 @@ export default async function PromotionDetailPage({ params }: { params: { pid: s
         <div className="flex flex-col items-center h-auto bg-[#FFF2F9]">
                 <div className="mt-16 w-[60vw] bg-pink-200 bg-flower rounded-lg flex flex-row justify-center p-[49px] gap-10 shadow-[0_4px_4px_-0px_rgba(250,78,171,1)]">
                     <div className="w-8/12 relative items-center">   
-                        <img className="rounded-lg" src={matchedPromotion ? matchedPromotion.picture : '/cars/fortuner.jpg'} alt="Promotion Image" />
+                        <img className="rounded-lg" src={matchedPromotion ? matchedPromotion.picture : '/cars/fortuner.jpg'} alt="Promotion Image" draggable={false} />
                     </div>  
                     <div className="w-4/12 relative items-start text-left text-pretty break-words">
                         <span className="text-black text-4xl font-bold font-['Lato']">{promotionDetail.data.name}<br/></span>
@@ -36,19 +36,26 @@ export default async function PromotionDetailPage({ params }: { params: { pid: s
                 <div className="w-[60vw] mt-12 text-left pl-[49px]">
                     <span className="text-black text-4xl font-normal font-['Lato']">Comments</span>
                 </div>
-                <div className="mt-8 bg-pink-200 bg-flower w-[60vw] rounded-lg flex flex-col p-[49px] gap-10 shadow-[0_4px_4px_-0px_rgba(250,78,171,1)]">
+                <div className="mt-8 bg-pink-200 bg-flower w-[60vw] rounded-lg flex flex-col p-[49px] gap-10 shadow-[0_4px_4px_-0px_rgba(250,78,171,1)] ">
                     {feedbackData && (
                             <div>
                                 {Promise.all(feedbackData.data.map(async (feedback: feedback) => {
                                     const profilePicData = await getProfilePicturebyId(feedback.user);
                                     const profilePic = profilePicData.data.profilePic || profilePicData.data ;
                                     return (
-                                        <div className="bg-white w-auto rounded-lg mt-4 flex flex-row" key={feedback._id}>
-                                            <img src={profilePic} alt="Profile" className="w-12 h-12 rounded-full" />
+                                        <div className="bg-white w-[55vw] rounded-lg mt-4 flex flex-row p-[25px]" key={feedback._id}>
+                                            <Image src={profilePic} alt="Profile" className="w-12 h-12 rounded-full mt-4 " width={0} height={0} draggable={false} />
                                             {/* {feedback.user} */}
-                                            {feedback.comment}
+                                            <div className="text-left ml-6">
+                                                <div className="text-xl">{feedback.username}</div>
+                                                <Star stars={feedback.rating}/>
+                                                <div className="text-bas text-wrap break-all">
+                                                    {feedback.comment}
+                                                </div>
+                                            </div>
                                         </div>
                                     );
+                                    
                                 }))}
                             </div>
                     )}
