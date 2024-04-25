599dc7d31cba32c65a71e2f1f24ffa3a483cff8a
bonkizu <isukaomu@gmail.com>
2024-04-25 16:17:02 +0700

    fix home and transition

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,28 +1,25 @@
 "use client";
+import { motion } from "framer-motion";
 
-import { motion, AnimatePresence } from "framer-motion";
-
-export default async function Transition({
+export default function Transition({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <AnimatePresence initial={true}>
-      <motion.div
-        initial={{ y: 30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: 30, opacity: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 260,
-          damping: 20,
-          duration: 0.75,
-          delay: 0.5,
-        }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 30, opacity: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        duration: 0.75,
+        delay: 0.5,
+      }}
+    >
+      {children}
+    </motion.div>
   );
 }
