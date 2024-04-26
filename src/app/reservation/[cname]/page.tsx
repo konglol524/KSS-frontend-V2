48e6d6749db0a686bf06082a47ea7a83eb255604
bonkizu <isukaomu@gmail.com>
2024-04-26 22:45:00 +0700

    booking scroll

--- src/app/reservation/[cname]/page.tsx
+++ src/app/reservation/[cname]/page.tsx
@@ -1,33 +1,37 @@
-import ReservationForm from "@/components/ReservationForm"
+import ReservationForm from "@/components/ReservationForm";
 import getCars from "@/libs/getCars";
 import getRentals from "@/libs/getRentals";
 import { authOptions } from "@/libs/auth";
 import { getServerSession } from "next-auth";
 import getBookings from "@/libs/getBookings";
 
-export default async function CarReservationPage () {
-    const shops:rentals = await getRentals();
-    
-    const session = await getServerSession(authOptions);
+export default async function CarReservationPage() {
+  const shops: rentals = await getRentals();
 
-    if(!session) return;
-    const bookings:Bookings = await getBookings(session.user.token);
-    
-    return (
-        <div>
-            <ReservationForm shops={shops} user={session.user} bookingsAmount={bookings.count}/>
-        </div>
-    );
+  const session = await getServerSession(authOptions);
+
+  if (!session) return;
+  const bookings: Bookings = await getBookings(session.user.token);
+
+  return (
+    <div className="max-h-[90vh] overflow-y-hidden">
+      <ReservationForm
+        shops={shops}
+        user={session.user}
+        bookingsAmount={bookings.count}
+      />
+    </div>
+  );
 }
 
 export async function generateStaticParams() {
-    const cars:{[key:string]: Car} = getCars(); 
+  const cars: { [key: string]: Car } = getCars();
 
-    const staticParamsArray:Array<{[cname:string]:string}> = [];
+  const staticParamsArray: Array<{ [cname: string]: string }> = [];
 
-    for(let cname in cars){
-        staticParamsArray.push({cname: cname});
-    }
+  for (let cname in cars) {
+    staticParamsArray.push({ cname: cname });
+  }
 
-    return staticParamsArray;
+  return staticParamsArray;
 }
