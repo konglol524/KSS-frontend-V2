2ad5ed645febbcda12b26ad3e30833c717af50f8
PiyawatArt <piyawat.manu@gmail.com>
2024-04-26 22:36:12 +0700

    refresh

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -2,10 +2,12 @@
 import React, { useState } from "react";
 import addFeedback from "@/libs/addFeedback";
 import { Rating} from '@mui/material';
+import { useRouter } from "next/navigation";
 
 const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
   const [comment, setComment] = useState("");
   const [rating, setRating] = useState<number | null>(5);
+  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -15,6 +17,7 @@ const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
       // If feedback is successfully added, clear the form and call onSuccess
       setComment("");
       setRating(0);
+      router.refresh();
     } catch (error) {
       console.error("Error adding feedback:", error);
       // Handle error
