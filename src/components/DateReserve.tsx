2e3889f82a87be87363381f936565f8e31c1c344
bonkizu <isukaomu@gmail.com>
2024-04-26 23:52:46 +0700

    booking v2

--- src/components/DateReserve.tsx
+++ src/components/DateReserve.tsx
@@ -1,17 +1,26 @@
-import { DatePicker   } from '@mui/x-date-pickers'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import dayjs from 'dayjs'
+import { DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
-export default function DateReserve({onDateChange, day}:{onDateChange:Function; day:any}) {
-    return (
-        <>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                value={day}
-                className='rounded-lg bg-white border-[#FA4EAB] border-2' 
-                format='DD/MM/YYYY' sx={{width: "100%" }} onChange={(value)=>onDateChange(value)}/>
-            </LocalizationProvider>
-            </>
-    )
+export default function DateReserve({
+  onDateChange,
+  day,
+}: {
+  onDateChange: Function;
+  day: any;
+}) {
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          value={day}
+          className="rounded-lg bg-white border-[#FA4EAB] border-2"
+          format="DD/MM/YYYY"
+          sx={{ width: "100%" }}
+          onChange={(value) => onDateChange(value)}
+        />
+      </LocalizationProvider>
+    </>
+  );
 }
