db0aa6bab89ff6dbbd44f446c8aafd6188c2fb50
khongkrapuns <khongkrapuns@gmail.com>
2024-04-26 15:27:00 +0700

    update rating cal and star size

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -2,18 +2,18 @@ import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 
-const Star = ({stars}:{stars:number;}) => {
+const Star = ({stars,fontsize}:{stars:number;fontsize:string;}) => {
     const ratingStar = Array.from({ length: 5 }, (elem, index) => {
         let number = index + 0.5;
         debugger;   
         return (
             <span key={index}>
                 {stars >= index + 1 ? (
-                    <StarIcon style={{ color: '#E4A33D' }}/>
+                    <StarIcon style={{ color: '#E4A33D', fontSize: fontsize}}/>
                 ) : stars >= number ? (
-                    <StarHalfIcon style={{ color: '#E4A33D' }}/>
+                    <StarHalfIcon style={{ color: '#E4A33D', fontSize: fontsize }}/>
                 ) : (
-                    <StarBorderIcon style={{ color: '#E4A33D' }}/>
+                    <StarBorderIcon style={{ color: '#E4A33D', fontSize: fontsize}}/>
                 )}
             </span>
         );
