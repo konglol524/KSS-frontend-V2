f7cf108358dd1fbfae0e759ab740ddd5e5b38d42
PiyawatArt <piyawat.manu@gmail.com>
2024-04-26 20:41:49 +0700

    feedback star

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React, { useState } from "react";
 import addFeedback from "@/libs/addFeedback";
+import { Rating} from '@mui/material';
 
 const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
   const [comment, setComment] = useState("");
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState<number | null>(5);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -21,24 +22,28 @@ const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <textarea
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        placeholder="Write your feedback..."
-      />
-      <input
-        type="number"
-        value={rating}
-        onChange={(e) => setRating(e.target.valueAsNumber)}
-        min={0}
-        max={5}
-        step={0.5}
-        placeholder="Rating (0-5)"
-      />
-      <button type="submit">Submit Feedback</button>
-    </form>
+    <div className="flex flex-row">
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <div className="relative">
+          <textarea
+            className="bg-white w-[50vw] rounded-lg mt-4 flex flex-row p-[25px] border border-gray-300"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Write your feedback..."
+          />
+          <div className="absolute bottom-0 right-0 mr-4 mb-4">
+            <Rating
+              name="simple-controlled"
+              value={rating}
+              onChange={(event, newValue) => {
+                setRating(newValue);
+              }}
+            />
+          </div>
+        </div>
+      </form>
+      <button type="submit" className="bg-[#FA4EAB] rounded-lg shadow-xl py-3 text-xl text-white ml-4 self-center mt-3">Submit Feedback</button>
+    </div>
   );
 };
-
 export default FeedbackForm;
