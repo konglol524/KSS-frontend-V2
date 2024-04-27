7a528c573b401d417903c64b56eee02f9f8788d1
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2024-04-27 16:22:49 +0700

    edit booking no finit yet

---
+++ src/components/PrommotionSelect.tsx
@@ -0,0 +1,30 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function PromotionSelect({
+  shops,
+  onShopChange,
+}: {
+  shops: rentals;
+  onShopChange: Function;
+}) {
+  return (
+    <Select onValueChange={(value) => onShopChange(value)}>
+      <SelectTrigger className="w-full border-2 border-pink-400">
+        <SelectValue placeholder="Select Provider" />
+      </SelectTrigger>
+      <SelectContent>
+        {shops.data.map((shop: rentalProvider) => (
+          <SelectItem key={shop._id} value={shop._id}>
+            {shop.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
