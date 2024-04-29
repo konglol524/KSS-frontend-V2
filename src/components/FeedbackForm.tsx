23d796c4a88bd1d0134f5729d8fd9a017b5f3f2f
Phuree <phureeppp@gmail.com>
2024-04-29 14:19:10 +0700

    change comment submit btn

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,11 +1,18 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import addFeedback from "@/libs/addFeedback";
-import { Rating } from '@mui/material';
+import { Rating } from "@mui/material";
 import { useRouter } from "next/navigation";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
+import { IoMdSend } from "react-icons/io";
 
-const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
+const FeedbackForm = ({
+  promoID,
+  token,
+}: {
+  promoID: string;
+  token: string;
+}) => {
   const [comment, setComment] = useState("");
   const [rating, setRating] = useState<number | null>(5);
   const router = useRouter();
@@ -26,36 +33,45 @@ const FeedbackForm = ({ promoID, token }:{promoID:string,token:string}) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-row w-[100%] h-[100%] ml-6">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-row w-[100%] h-[100%] ml-6"
+    >
       <div className="w-[90%] h-[100%] text-left">
-            <Rating
-              name="simple-controlled"
-              value={rating}
-              
-              precision={0.5}
-              onChange={(event, newValue) => {
-                setRating(newValue);
-              }}
-            />
-            <TextField
-              className="bg-white w-[100%] border border-gray-300"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder="Write your feedback..."
-              id="standard-basic" variant="standard" fullWidth  
-              inputProps={{
-                style: {
-                  padding: 0
-                }
-            }}
-            />
+        <Rating
+          name="simple-controlled"
+          value={rating}
+          precision={0.5}
+          onChange={(event, newValue) => {
+            setRating(newValue);
+          }}
+        />
+        <TextField
+          className="bg-white w-[100%] border border-gray-300"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Write your feedback..."
+          id="standard-basic"
+          variant="standard"
+          fullWidth
+          inputProps={{
+            style: {
+              padding: 0,
+            },
+          }}
+        />
       </div>
-          <div className="w-[10%] justify-center items-center">
-            <button type="submit" className="bg-[#FA4EAB] w-[90%] h-[100%] rounded-lg shadow-xl text-center text-sm text-white">POST</button>
-          </div>
+      <div className="flex justify-center items-center w-[10%]">
+        <button
+          title="send"
+          type="submit"
+          className="bg-[#FA4EAB] rounded-lg shadow-xl text-center text-sm text-white w-9 h-9 flex items-center justify-center duration-200 hover:opacity-80 hover:scale-110"
+        >
+          <IoMdSend />
+        </button>
+      </div>
     </form>
   );
-  
 };
 
 export default FeedbackForm;
